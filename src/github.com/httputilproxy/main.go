81a62da9db4d3ec59f637a76ded85558416b2459
Nino Kodabande <ninok@hpe.com>
2016-11-25 12:42:09 -0800

    Removes transport and adds go max procs

--- src/github.com/httputilproxy/main.go
+++ src/github.com/httputilproxy/main.go
@@ -2,16 +2,15 @@ package main
 
 import (
 	"log"
-	"net"
 	"net/http"
 	"net/http/httputil"
 	"os"
-	"time"
+	"runtime"
 )
 
 func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
 	reverseproxy := &httputil.ReverseProxy{
-		Transport: transport,
 		Director: func(request *http.Request) {
 			request.URL.Scheme = "http"
 			request.URL.Host = "10.0.1.2:8080"
@@ -25,17 +24,4 @@ func main() {
 		log.Fatal(err)
 		os.Exit(1)
 	}
-}
-
-var transport = &http.Transport{
-	Dial: func(network, addr string) (net.Conn, error) {
-		conn, err := net.DialTimeout(network, addr, 5*time.Second)
-		if err != nil {
-			return conn, err
-		}
-		err = conn.SetDeadline(time.Now().Add(900 * time.Second))
-		return conn, err
-	},
-	DisableKeepAlives:  true,
-	DisableCompression: true,
 }
