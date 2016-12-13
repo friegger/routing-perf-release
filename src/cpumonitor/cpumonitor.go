9e341d7d719541f3e384cd8facc32235c8e274d1
Aaron Hurley <ahurley@pivotal.io>
2016-12-13 14:23:50 -0800

    WIP-initial setup
    [#135887919]
    
    Signed-off-by: Nino kodabande <ninok@hpe.com>

--- src/cpumonitor/cpumonitor.go
+++ src/cpumonitor/cpumonitor.go
@@ -9,6 +9,20 @@ import (
 	"github.com/shirou/gopsutil/cpu"
 )
 
+// TODO
+// security??
+// have some sort of chan of percentage
+// trigger start, either via a handler or something
+// get results, either via a handler or something
+
+func start(w http.ResponseWriter, r *http.Request) {
+	// start a goroutine for percentage
+}
+
+func stop(w http.ResponseWriter, r *http.Request) {
+	// return the serialized stuff
+}
+
 func getCPU(w http.ResponseWriter, r *http.Request) {
 	percent, err := cpu.Percent(1*time.Second, false)
 	if err != nil {
