c586c98a481faa3a109f51b602a8b73a5f02f7fc
Nino Kodabande <ninok@hpe.com>
2016-12-14 08:49:53 -0800

    WIP add stats collector
    [#135887919]

--- src/cpumonitor/stats/stats.go
+++ src/cpumonitor/stats/stats.go
@@ -1,44 +1,59 @@
 package stats
 
-import (
-	"net/http"
-	"time"
-)
+import "time"
 
 type Info struct {
 	Percentage []float64
 	TimeStamp  time.Time
 }
 
-//go:generate counterfeiter -o ../fakes/fake_cpustats.go . CPUStats
+//go:generate counterfeiter -o ../fakes/fake_cpu.go . cpu
 
-type CPUStats interface {
-	Run() error
-	Collect() ([]Info, error)
-	Stop() error
+type cpu interface {
+	Percent(time.Duration, bool) ([]float64, error)
 }
 
 type Collector struct {
+	interval    time.Duration
+	cpuInterval time.Duration
+	cpu         cpu
+	data        chan []Info
+	stop        chan struct{}
 }
 
-func NewCollector() *Collector {
-	return &Collector{}
-}
-
-type Handler struct {
-	cpuStats CPUStats
-}
-
-func NewStatHandler(c CPUStats) *Handler {
-	return &Handler{
-		cpuStats: c,
+func NewCollector(c cpu, interval, cpuInterval time.Duration) *Collector {
+	return &Collector{
+		interval:    interval,
+		cpuInterval: cpuInterval,
+		cpu:         c,
+		data:        make(chan []Info),
+		stop:        make(chan struct{}),
 	}
 }
 
-func (c *Handler) Start(w http.ResponseWriter, r *http.Request) {
-	// TODO: will return an error so we should handle that case
-	err := c.cpuStats.Run()
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
+func (c *Collector) Run() error {
+	ticker := time.NewTicker(c.interval)
+	var result []Info
+	for {
+		select {
+		case <-ticker.C:
+			percentage, err := c.cpu.Percent(c.cpuInterval, false)
+			if err != nil {
+				return err
+			}
+			result = append(result, Info{
+				Percentage: percentage,
+				TimeStamp:  time.Now(),
+			})
+		case <-c.stop:
+			c.data <- result
+			return nil
+		}
 	}
+	return nil
+}
+
+func (c *Collector) Result() []Info {
+	c.stop <- struct{}{}
+	return <-c.data
 }
